0bd7c82c3ffe33e2fef44a4c679a8935a22953c2
devildog1918 <robert.perryman27@gmail.com>
2026-04-05 18:37:24 -0500

    Update service worker cache version and add icons

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,26 @@
-const CACHE = "field-tech-v1";
+const CACHE = "field-tech-v2";
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
       cache.addAll([
         "./",
         "./index.html",
-        "./manifest.json"
+        "./manifest.json",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
       ])
     )
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
